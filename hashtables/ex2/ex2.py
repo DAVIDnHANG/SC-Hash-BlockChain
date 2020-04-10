eb5dfd1f3a8c91609960b281c16691c115d4a6fb
DAVID HANG <davidnhang@gmail.com>
2020-04-10 09:03:57 -0700

    reconstruct_trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,27 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    # algo For
+    # Base Case: find the starting case None, such that ///  (None=i_0, destination=j_0).
+    # case_1: for next we find i_1 such that i_1 = j_0, ///  (i_1,     j_1)
+    # so that means,                        i_2 = j_1  /// (i_2,     j_2)
+    # hash each ticket st  hash(key) = source, hash(value) = destination.
+    #base case: at index=0, we look for source = NONE,
+    # When we call hash_table_retrieve on (key)=None, it returns the hash(value) = destination=j_0,
+    #then we assign it to route[index=0] = destination=j_0 at index=0,
+    #now on index=1, we can use hash_table_retrieve(hashtable ,route[i=0]) = route[index=1] to call destination=j_1.
+
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    pass
+    for index in range(length):
+        if index == 0:
+            route[index] = hash_table_retrieve(hashtable, "NONE")
+        else:
+            previousValue = route[index-1]
+            route[index] = hash_table_retrieve(hashtable, previousValue)
+    return route[0:-1]
+
