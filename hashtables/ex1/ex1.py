3bb415e00f279ece50635d43993b5a3dfae2177e
DAVID HANG <davidnhang@gmail.com>
2020-04-10 09:04:58 -0700

    get_indices_of_item_weights
    done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,24 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
+    # check if length is one or less, return None.
+    # insert the first index,value into the hash_table.
+    # second loop:
+    # then do math difference=limit-Value at Index.
+    # use the difference to retrieve the hash table.
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
-    return None
+    if length <= 1:
+        return None
+    for index, value in enumerate(weights):  # enumerate is to call index, and value of array
+        difference = limit - value
+        Hashretrieve = hash_table_retrieve(ht, difference)
+        if Hashretrieve is None:
+            hash_table_insert(ht, value, index)
+        else:
+            if index >= Hashretrieve:
+                return (index, Hashretrieve)
+            else:
+                return (Hashretrieve, index)
 
 
 def print_answer(answer):
